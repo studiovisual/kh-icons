c9981f336319dcdf40536da893f37589e963ba31
Sergio Menezes <smenezes@studiovisual.com.br>
2024-03-05 15:39:44 -0300

    Adicionar referencia do icone

--- build/react-icons/index.js
+++ build/react-icons/index.js
@@ -363,6 +363,8 @@ var _interrogation = _interopRequireDefault(require("./solid/interrogation"));
 
 var _x = _interopRequireDefault(require("./solid/x"));
 
+var _help = _interopRequireDefault(require("./solid/help"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
